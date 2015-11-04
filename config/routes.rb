f021cd67152d66a40263dce6da3f021830439a7f
Federico <federico.arrua.26@outlook.com>
2015-11-04 15:37:16 -0300

    Pantalla de iniciar sesion

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/login' , to: 'login#index', as: 'login'
+
   root 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
