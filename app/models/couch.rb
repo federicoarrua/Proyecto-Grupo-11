912c47a7d2387f16a4aa172ea78d8af1137acdc9
Federico <federico.arrua.26@outlook.com>
2015-11-24 01:22:33 -0300

    paperclip

--- app/models/couch.rb
+++ app/models/couch.rb
@@ -1,6 +1,9 @@
 class Couch < ActiveRecord::Base
 	belongs_to :tipo
 	belongs_to :user
+
+	attr_accessible :foto
+    has_attached_file :foto, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 end
 
 
