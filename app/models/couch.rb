efcf0124552ad26a11cc433b09d8ad348be19e9a
Federico <federico.arrua.26@outlook.com>
2015-11-24 02:42:09 -0300

    Rollback paperclip

--- app/models/couch.rb
+++ app/models/couch.rb
@@ -1,9 +1,6 @@
 class Couch < ActiveRecord::Base
 	belongs_to :tipo
 	belongs_to :user
-
-	attr_accessible :foto
-    has_attached_file :foto, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 end
 
 
